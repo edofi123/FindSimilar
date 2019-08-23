587cc570fc972da714179b5c97213bb5dc977407
edofi <edofi123@gmail.com>
2019-08-23 13:23:29 +0300

    few changes

--- doublePicture/findDuplicates.py
+++ doublePicture/findDuplicates.py
@@ -4,11 +4,16 @@ from PIL import Image
 
 
 '''Const variables'''
-PTC_MUL = 3
-PR_START = 40
-PR_MUL = 5
+PTC_MUL = 2
+PTC_START = 5
+PR_START = 30
+PR_MUL = 4
 MIN = 1
 MAX = 5
+
+
+def rand(min_val, max_val):
+    return random.randint(min_val, max_val)
 
 
 def tuple_calc(my_tuple, num, state):
@@ -22,6 +27,13 @@ def tuple_between(first, second, third):
 
 def check_image(file):
     return "jpg" in str(file) or "png" in str(file) or "jpeg" in str(file)
+
+
+def print_images_name(image_list):
+    for image in image_list:
+        print(str(image_list.index(image) + 1) + ". " + image)
+    print()
+    return input()
 
 
 def starting_program():
@@ -45,34 +57,46 @@ def show_similar(path, similar_dict):
     Output: nothing
     """
 
+    print("I found " + str(len(similar_dict)) + " images that match to other images")
     removed_images = []
+    # removed_images_size = 0
     for file, similar in similar_dict.items():
         if file in removed_images:
             continue
         for image in [file] + similar:
+            if image in removed_images:
+                similar.remove(image)
+                continue
             os.startfile(path + "\\" + image)
         print("Do you want to remove one of the images? (y/n)")
         ans = input()
         if ans.lower() == 'y':
             print("Choose one of the options:\n"
                   "1. Remove one or couple of images\n"
-                  "2. Remove all images")
+                  "2. Remove all images except one or couple of them\n"
+                  "3. Remove all images")
             ans = input()
-            list_of_images = []
+            list_of_images = [file] + similar
+            list_of_indexes = []
             if ans == "1":
-                print("Which images would you like to remove? (leave a space between two names)")
-                for image in [file] + similar:
-                    print(image)
-                images_to_remove = input()
-                list_of_images = images_to_remove.split(' ')
+                print("Which images would you like to remove? (leave a space between two numbers)")
+                images_to_remove = print_images_name(list_of_images)
+                list_of_indexes = images_to_remove.split(' ')
             elif ans == "2":
-                list_of_images = [file] + similar
-            for image in list_of_images:
-                os.remove(path + "\\" + image)
-                if image in similar_dict:
-                    removed_images.append(image)
-            if file in list_of_images:
-                continue
+                print("Which images would you like to keep? (leave a space between two numbers)")
+                images_to_keep = print_images_name(list_of_images)
+                list_of_indexes = list(range(len(list_of_images)))
+                for index in images_to_keep.split(' '):
+                    if (int(index) - 1) in list_of_indexes:
+                        list_of_indexes.remove(int(index) - 1)
+            elif ans == "3":
+                list_of_indexes = list(range(len(list_of_images)))
+            for index in list_of_indexes:
+                if (int(index) - 1) in list(range(len(list_of_images))):
+                    os.remove(path + "\\" + list_of_images[int(index) - 1])
+                    if image in similar_dict:
+                        removed_images.append(image)
+    print("That's it!")
 
 
 def search_similar(path, accuracy):
@@ -84,33 +108,37 @@ def search_similar(path, accuracy):
 
     if not (MIN <= accuracy <= MAX):
         accuracy = MIN
-    pixels_to_check = accuracy * PTC_MUL
+    pixels_to_check = accuracy * PTC_MUL + PTC_START
     pixels_range = PR_START - (accuracy * PR_MUL)
     checked_files = []
     similar_images = {}
     open_files = {}
+    rand_pixel = []
     dir_files = os.listdir(path)
     # Open all the picture and take details
+    print("Loading part 1...")
+    for num in range(pixels_to_check):
+        rand_pixel.append(rand(10, 100) / 10)
     for file in dir_files:
         if check_image(file):
             info = Image.open(path + '\\' + file)
             pixels = info.load()
             pixels_list = []
             # Choose pixels to examine
-            for num in range(1, pixels_to_check):
-                x = (info.width / num) - 1
-                y = (info.height / num) - 1
+            for num in rand_pixel:
+                x = int((info.width / num) - 1)
+                y = int((info.height / num) - 1)
                 pixels_list.append(pixels[x, y])
             open_files[file] = (info.size, pixels_list)
             info.close()
-            print(file)
     # Go over all the files in the directory and compare each file to the others
+    print("Loading part 2...")
     for name1, info1 in open_files.items():
         for name2, info2 in open_files.items():
             # Check: Not the same file, name2 hasn't checked yet
             if (name1 == name2) or (name2 in checked_files):
                 continue
-            if info1[0] == info2[0]:
+            if info1[0][0] == info2[0][0] and info1[0][1] == info2[0][1]:
                 similar = True
                 # Compare the pixels
                 for x in range(len(info1[1])):
@@ -124,13 +152,13 @@ def search_similar(path, accuracy):
                         similar_images[name1] = [name2]
         # Do not double check each file
         checked_files.append(name1)
-        print(name1)
     return similar_images
 
 
 def main():
     path, accuracy = starting_program()
     show_similar(path, search_similar(path, accuracy))
+    input()
 
 
 main()
